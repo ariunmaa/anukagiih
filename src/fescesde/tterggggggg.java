8a46ec5b5cf0563b11697cb0c7fa7f3ff440468b
Admin <Admin@DESKTOP-53KNEMD>
2023-04-12 14:35:01 +0800

    erwarfqw

---
+++ src/fescesde/tterggggggg.java
@@ -0,0 +1,17 @@
+package fescesde;
+
+public class tterggggggg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		      int[] myNumbers = {1, 2, 3};
+		      System.out.println(myNumbers[10]);
+		    } catch (Exception e) {
+		      System.out.println("Something went wrong.");
+		    } finally {
+		      System.out.println("The 'try catch' is finished.");
+		    }
+	}
+
+}
